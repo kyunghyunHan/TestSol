9ed070e9be8d5094bdc6bcc2cd8f7317a045c5ce
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-08-02 16:50:12 +0900

    ㅁㅇㅇ

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,19 @@
-const Migrations = artifacts.require("Migrations");
+const { deployProxy } = require("@openzeppelin/truffle-upgrades");
+const ImageNFT = artifacts.require("ImageNFT");
+const MarketPlace = artifacts.require("MarketPlace");
+const Lib = artifacts.require("Lib");
+module.exports = async function (deployer) {
+  const ImageNFTIns = await deployProxy(ImageNFT, {
+    deployer
+  });
+  const maininstance = await deployProxy(MarketPlace, [ImageNFTIns.address], {
+    deployer
+  });
+  const libinstance = await deployProxy(Lib, {
+    deployer
+  });
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  console.log("Deployed", ImageNFTIns.address);
+  console.log("Deployed", maininstance.address);
+  console.log("Deployed", libinstance.address);
 };
