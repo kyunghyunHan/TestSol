4888e6396895e7ea36c0ba7ef83ba7624b36a752
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-08-12 17:00:29 +1000

    feat:update

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,20 +1,18 @@
 // SPDX-License-Identifier: MIT
-// compiler version must be greater than or equal to 0.8.26 and less than 0.9.0
 
 pragma solidity ^0.8.26;
 
-contract Counter{
+contract Counter {
     uint256 public count;
-    
+
     function get() public view returns (uint256){
         return count;
     }
 
-    function inc() public {
+    function inc()public {
         count +=1;
     }
-
-    function dec() public {
+    function dec()public{
         count -=1;
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
+//Test
 contract CounterTest is Test {
     Counter public counter;
-
+    // 1 . first setUP
     function setUp() public {
         counter = new Counter();
     }
-
+ 
     function test_Increment() public {
         counter.inc();
         assertEq(counter.get(), 1);
